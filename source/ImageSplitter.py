0f07dc360f12dbe976fb714ca3808ea82edd2505
Busetty-Charan <125012012@sastra.ac.in>
2024-03-24 11:46:53 +0530

    Add more directories to ignore, Rework on ImageSplitter to use different library

--- source/ImageSplitter.py
+++ source/ImageSplitter.py
@@ -3,48 +3,15 @@ import pickle
 import zlib
 
 import cv2
-import dlib
+import face_recognition
 
-# * ImageSplitter uses 8-bit color depth for any image fed into it
 class ImageSplitter:
-    """
-    ImageSplitter is a utility class that helps extract individual faces
-    from an RGB image containing faces of multiple people present with
-    their faces turned to the camera
-
-    * Implements functions provided by dlib library to detect faces and crop
-    * out the detected faces
-    """
-
-    def __init__(self):
-
-        self.face_detector = dlib.get_frontal_face_detector()
 
     def getFaceBoundingBoxes(self, rgb_image):
-        """
-        getFaceBoundingBoxes takes in an RGB Image and spits out multiple
-        rectangular bounding boxes containing just the face of each person
-        detected in the image
-
-        Params:
-            rgb_image:
-                numpy.ndarray : A four dimensional array containing rgb image data
-                obtained from dlib.load_rgb_image function
-
-
-        Returns:
-            ! Returns empty collection on error
-
-            dlib.rectangles : A collection of dlib.rectangle objects which
-            enclose bounds of every face detected in the image
-
-        """
-
-        assert rgb_image is not None
 
         try:
 
-            return self.face_detector(rgb_image, 1)
+            return face_recognition.face_locations(rgb_image)
 
         except Exception as e:
 
@@ -53,24 +20,6 @@ class ImageSplitter:
             return []
 
     def saveDetectedFaces(self, image_path, save_path):
-        """
-        getDetectedFaces takes in a path to an RGB image and saves the detected
-        faces into the filesystem
-
-        Params:
-            image_path:
-                str : Path to an RGB image in the filesystem
-
-            save_path:
-                str: Path to a folder in the filesystem to save detected faces
-
-        Returns:
-            ! Returns a value of -1 when image path is invalid
-            ! Returns a value of -2 when save path is invalid
-        """
-
-        assert image_path is not None
-        assert save_path is not None
 
         try:
 
@@ -98,12 +47,12 @@ class ImageSplitter:
 
             return -2
 
-        rgb_image = dlib.load_rgb_image(image_path)
+        rgb_image = face_recognition.load_image_file(image_path)
         detected_faces = self.getFaceBoundingBoxes(rgb_image)
 
         for face in detected_faces:
 
-            extracted_face = dlib.sub_image(rgb_image, face)
+            extracted_face = rgb_image[face[0]:face[2], face[3]:face[1]]
             crc_hash = zlib.crc32(pickle.dumps(face) + pickle.dumps(extracted_face))
 
             cv2.imwrite(f"{save_path}/face_{crc_hash}.jpeg", extracted_face)
