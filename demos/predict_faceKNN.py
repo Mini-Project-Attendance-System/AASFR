4d6a97e220bf220b97d8ceda2351413a36750f24
Busetty-Charan <125012012@sastra.ac.in>
2024-04-26 17:32:33 +0530

    modified:   demos/predict_faceKNN.py modified:   demos/predict_faceSVM.py
    Added confusion matrix generation

--- demos/predict_faceKNN.py
+++ demos/predict_faceKNN.py
@@ -1,6 +1,6 @@
 from os import listdir, path
 
-from sklearn.metrics import classification_report, accuracy_score
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
 
 from source import KNNTrainer, Logger
 
@@ -29,3 +29,7 @@ if __name__ == "__main__":
 
     logger.info(f"Accuracy score : {accuracy_score(labels, predictions)}")
     logger.info(f"Classification report :\n{classification_report(labels, predictions)}")
+
+    matrix = confusion_matrix(labels, predictions)
+
+    logger.info(f"Confusion matrix :\n{matrix}")
--- demos/predict_faceSVM.py
+++ demos/predict_faceSVM.py
@@ -1,6 +1,6 @@
 from os import listdir, path
 
-from sklearn.metrics import classification_report, accuracy_score
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
 
 from source import SVMTrainer, Logger
 
@@ -15,13 +15,11 @@ if __name__ == "__main__":
     root = "testing_images"
 
     predictions = []
-    labels = []
+    labels = predictor.classes
 
     for directory in listdir(root):
 
         for image in listdir(path.join(root, directory)):
-
-            labels.append(directory)
 
             prediction = predictor.predictFace(path.join(root, directory, image), "trained_models/vakibcSVM.clf")
 
@@ -29,3 +27,7 @@ if __name__ == "__main__":
 
     logger.info(f"Accuracy score : {accuracy_score(labels, predictions)}")
     logger.info(f"Classification report :\n{classification_report(labels, predictions)}")
+
+    matrix = confusion_matrix(labels, predictions)
+
+    logger.info(f"Confusion matrix :\n{matrix}")
